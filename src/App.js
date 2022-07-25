cb3d0001db057ef59c18952eb2d41592bb32e353
SathyaRaj <sathyarajsaliyan@gmail.com>
2022-07-26 00:21:49 +0530

    responsive landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import './key';
 import Axios from "axios";
+import LandingPage from './Components/LandingPage';
+import FoodDisplay from './Components/FoodDisplay';
 function App() {
   
   const API_KEY="7fe8c8505c1246ea9b24a76aff3eded8"
-  
   var url=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=chicken&maxFat=25&number=2`
  async function getRecipes(){
     var result= await Axios.get(url);
@@ -13,8 +13,9 @@ function App() {
   }
 
   return (
-    <div className="App" onClick={getRecipes} >
-      heloo
+    <div className="App scrollbar-hide "  >
+      <LandingPage/>
+      <FoodDisplay/>
     </div>
   );
 }
