a22aab58a9fcbcc40e673618b90b062152e13101
SathyaRaj <sathyarajsaliyan@gmail.com>
2022-07-25 10:53:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './key';
+import Axios from "axios";
 function App() {
+  
+  const API_KEY="7fe8c8505c1246ea9b24a76aff3eded8"
+  
+  var url=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=chicken&maxFat=25&number=2`
+ async function getRecipes(){
+    var result= await Axios.get(url);
+    console.log(result.data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={getRecipes} >
+      heloo
     </div>
   );
 }
