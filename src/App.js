4ce0995d7168216343d5046c006c6ab79535b575
SathyaRaj <sathyarajsaliyan@gmail.com>
2022-07-28 00:17:13 +0530

    functional and a responsive search bar

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import LandingPage from './Components/LandingPage';
 import FoodDisplay from './Components/FoodDisplay';
 function App() {
   
-  const API_KEY="7fe8c8505c1246ea9b24a76aff3eded8"
-  var url=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=chicken&maxFat=25&number=2`
- async function getRecipes(){
-    var result= await Axios.get(url);
-    console.log(result.data);
-  }
+  // const API_KEY="7fe8c8505c1246ea9b24a76aff3eded8"
+  // var url=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=chicken&maxFat=25&number=2`
+  
 
   return (
-    <div className="App scrollbar-hide "  >
+    <div className="App "   >
       <LandingPage/>
       <FoodDisplay/>
     </div>
