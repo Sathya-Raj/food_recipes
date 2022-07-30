49d98277269c2ed3fd1514e8b1a8aca5d8399f18
SathyaRaj <sathyarajsaliyan@gmail.com>
2022-07-30 15:27:19 +0530

    food items displayed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 
 import './App.css';
-import Axios from "axios";
 import LandingPage from './Components/LandingPage';
 import FoodDisplay from './Components/FoodDisplay';
-function App() {
-  
-  // const API_KEY="7fe8c8505c1246ea9b24a76aff3eded8"
-  // var url=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=chicken&maxFat=25&number=2`
-  
 
-  return (
+function App() {
+
+  
+  return (    
     <div className="App "   >
       <LandingPage/>
-      <FoodDisplay/>
+      <FoodDisplay />
     </div>
   );
 }
